ddbc25d070fac1977f7d815d87c50c988d6da818
TermiDD <TermiDD@gmail.com>
2022-11-22 19:52:36 +0300

    complete task 4

---
+++ Seminar_1/Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && b > c) 
+    Console.Write("max = " + a);
+if (b > a && a > c) 
+    Console.Write("max = " + b);
+if (c > b && b > a) 
+    Console.Write("max = " + c);
