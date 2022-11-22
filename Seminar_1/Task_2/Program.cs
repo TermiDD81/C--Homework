3e0354b1a87be2dd2c3439910ba3cf583c8e8ef1
TermiDD <TermiDD@gmail.com>
2022-11-22 19:17:47 +0300

    task 2 complete

---
+++ Seminar_1/Task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n1 > n2)
+    Console.Write("max = " + n1);
+else 
+    Console.Write("max = " + n2);
