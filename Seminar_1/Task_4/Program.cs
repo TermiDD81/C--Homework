2c8ba5ca71ffc45910979b724571739e52a75f52
TermiDD <TermiDD@gmail.com>
2022-11-23 19:03:41 +0300

    rework task 4

--- Seminar_1/Task_4/Program.cs
+++ Seminar_1/Task_4/Program.cs
@@ -5,9 +5,9 @@ Console.Write("Введите число 2: ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число 3: ");
 int c = Convert.ToInt32(Console.ReadLine());
-if (a > b && b > c) 
-    Console.Write("max = " + a);
-if (b > a && a > c) 
-    Console.Write("max = " + b);
-if (c > b && b > a) 
-    Console.Write("max = " + c);
+int max = a;
+if (b > max) 
+    max = b;
+if (c > max) 
+    max = c;
+Console.Write("max = " + max);
