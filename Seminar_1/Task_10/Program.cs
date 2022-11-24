0ba2bb54abf1bc58fb4cefe46cd6c81aa8b3b5e7
TermiDD <TermiDD@gmail.com>
2022-11-24 23:18:58 +0300

    complete task 10

---
+++ Seminar_1/Task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите 3-х значное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a < 100 || a > 999)
+{
+    Console.WriteLine("Вы ошиблись!\nВведите 3-х значное число: ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+if (a > 99 && a < 1000)
+    Console.Write((a / 10) % 10);
