5d94a3a42b25312e92d06e05b1e7c185d6d4edd3
TermiDD <TermiDD@gmail.com>
2022-11-24 23:26:38 +0300

    complete task 15

---
+++ Seminar_1/Task_15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите день недели (число): ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a < 1 || a > 7)
+{
+    Console.WriteLine("Вы ошиблись!\nВведите день недели (число): ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+if (a == 6 || a == 7)
+    Console.Write("Да");
+else 
+    Console.Write("Нет");
