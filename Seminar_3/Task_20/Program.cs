372c8dbba75f29dccc48cbc1f9980fd77b8f83b6
TermiDD <TermiDD@gmail.com>
2022-11-28 23:07:15 +0300

    complete task 21

--- Seminar_3/Task_20/Program.cs
+++ Seminar_3/Task_20/Program.cs
@@ -2,11 +2,9 @@
 Console.WriteLine("Введите координату точки A: ");
 double xa = Convert.ToDouble(Console.ReadLine());
 double ya = Convert.ToDouble(Console.ReadLine());
-double za = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите координату точки B: ");
 double xb = Convert.ToDouble(Console.ReadLine());
 double yb = Convert.ToDouble(Console.ReadLine());
-double zb = Convert.ToDouble(Console.ReadLine());
 
-double result = Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2) + Math.Pow(za - zb, 2));
-Console.WriteLine($"Расстояние между точками: {Math.Round(result, 2)}");
+double result = Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2));
+Console.WriteLine($"Расстояние между точками: {Math.Round(result, 3)}");
