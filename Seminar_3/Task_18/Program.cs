cce1962a22d186ab24e6e5dabb506336ae0b7ab9
TermiDD <TermiDD@gmail.com>
2022-11-28 21:23:28 +0300

    true folder

---
+++ Seminar_3/Task_18/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите номер четверти: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (x == 1)
+    Console.WriteLine("x > 0 && y > 0");
+else if (x == 2)
+    Console.WriteLine("x < 0 && y > 0");
+else if (x == 3)
+    Console.WriteLine("x < 0 && y < 0");
+else
+    Console.WriteLine("x > 0 && y < 0");
+
---
+++ Seminar_3/Task_21/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координату точки A: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+double ya = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату точки B: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+double yb = Convert.ToDouble(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2));
+Console.WriteLine($"Расстояние между точками: {Math.Round(result, 3)}");
