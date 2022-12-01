eb5e0b0e4084aa45d968ad8af59deafa0f402325
TermiDD <TermiDD@gmail.com>
2022-12-01 23:02:07 +0300

    complete task 25

---
+++ Seminar_3/Dop_task/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+void FillArray(int[] gryadki)
+{
+    Console.WriteLine("Введите количество грядок: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int length = gryadki.Length;
+    int index = 0;
+    while (index < length)
+    {
+        gryadki[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
---
+++ Seminar_4/Task_25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int s = 1;
+int i = b;
+while (i > 0)
+{
+    s = s * a;
+    i--;
+}
+Console.WriteLine("А в степени В = ");
+Console.WriteLine(s);
