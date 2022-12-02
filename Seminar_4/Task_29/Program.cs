75c4d4aa5c5d43843a9b81718407014c65b418ce
TermiDD <TermiDD@gmail.com>
2022-12-02 22:59:30 +0300

    complete task 29

---
+++ Seminar_4/Task_29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите массив из 8 чисел: ");
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите число {i+1}");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
