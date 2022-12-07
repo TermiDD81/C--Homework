e51ffa42e782e6693b00e54b5203ca584c050bd7
TermiDD <TermiDD@gmail.com>
2022-12-07 20:14:30 +0300

    complete task 34

---
+++ Seminar_5/Task_24/Program.cs
@@ -0,0 +1,24 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 123);
+}
+
+int ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+            count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[123];
+InputArray(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine(ReleaseArray(array));
---
+++ Seminar_5/Task_34/Program.cs
@@ -0,0 +1,25 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+            count++;
+        }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+CountEven(array);
+Console.WriteLine(CountEven(array));
