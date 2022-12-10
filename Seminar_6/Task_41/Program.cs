fa33522b1ef3c0020b5aaa5bae9077653a8f2286
TermiDD <TermiDD@gmail.com>
2022-12-10 22:16:57 +0300

    complete task 41

---
+++ Seminar_6/Task_41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Сколько чисел будете вводить?: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int[] array = new int[m];
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine($"Введите число {i+1}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0)
+        count++;
+}
+Console.WriteLine($"Введенные числа: [{string.Join(", ", array)}]");
+Console.WriteLine($"Количество чисел > 0 = {count}");
