f626a8f0b70f738d5e8955171065778d29c78269
TermiDD <TermiDD@gmail.com>
2022-12-17 21:16:26 +0300

    complete task 54

---
+++ Seminar_8/Task_54/Program.cs
@@ -0,0 +1,44 @@
+﻿void InputMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11); 
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 1; k < matrix.GetLength(1); k++)
+            {
+               int l = k;
+            while (l > 0 && matrix[i, l - 1] < matrix[i, l])
+            {
+                int tmp = matrix[i, l - 1];
+                matrix[i, l - 1] = matrix[i, l];
+                matrix[i, l] = tmp;
+                l -= 1;
+            }
+            }
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+InputMatrix(matrix);
+Console.WriteLine();
+SortMatrix(matrix);
