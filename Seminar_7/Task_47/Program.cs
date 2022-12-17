f1926750929deadf49d95ef625c73e45f03dac54
TermiDD <TermiDD@gmail.com>
2022-12-17 12:55:11 +0300

    complete task 47

---
+++ Seminar_7/Task_47/Program.cs
@@ -0,0 +1,18 @@
+﻿void InputMatrix(double[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 100)/10.0;
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+double[,] matrix = new double[coord[0], coord[1]];
+InputMatrix(matrix);
