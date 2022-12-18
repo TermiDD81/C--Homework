9203026dc1c49be085b8c24b4a1b3f478587cd47
TermiDD <TermiDD@gmail.com>
2022-12-18 10:33:50 +0300

    complete task 60

---
+++ Seminar_8/Task_60/Program.cs
@@ -0,0 +1,26 @@
+﻿void Input3DMatrix(int[, , ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int val1 = i;
+        Console.WriteLine($"Страница {i + 1}");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int val2 = j;
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                int val3 = k;
+                matrix[i, j, k] = new Random().Next(0, 11); 
+                Console.Write($"{matrix[i, j, k]} {(val1, val2, val3)} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+int[, , ] matrix = new int[coord[0], coord[1], coord[2]];
+Input3DMatrix(matrix);
