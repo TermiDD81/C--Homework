4a75060320b0e843b8c0834a362339aac9dc5a46
TermiDD <TermiDD@gmail.com>
2022-12-25 12:01:08 +0300

    complete task 68

---
+++ Seminar_9/Task_68/Program.cs
@@ -0,0 +1,15 @@
+﻿int rec(int m, int n)
+{
+    if (m == 0) 
+        return n + 1;
+    if (m != 0 && n == 0) 
+        return rec(m - 1, 1);
+    if (m > 0 && n > 0) 
+        return rec(m - 1, rec(m, n - 1));
+    return rec(m,n);
+}
+
+Console.Clear();
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"A({m},{n}) = {rec(m, n)}");
